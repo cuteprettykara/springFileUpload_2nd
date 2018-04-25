87b2ca7414af27e7e727586dd460bc41aabb5b7c
cuteprettykara <@gmail.com>
2018-04-25 11:58:41 +0900

    1_1_일반적인 파일 업로드

---
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,54 @@
+package org.zerock.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+	@Resource(name="uploadPath")
+	private String uploadPath;
+
+	@RequestMapping(value="/uploadForm", method=RequestMethod.GET)
+	public void uploadForm() {
+	}
+	
+	@RequestMapping(value="/uploadForm", method=RequestMethod.POST)
+	public String uploadForm(MultipartFile file, Model model) throws IOException {
+		logger.info("originalName: {}", file.getOriginalFilename());
+		logger.info("size: {}", file.getSize());
+		logger.info("contentType: {}", file.getContentType());
+		
+		String savedName= uploadFile(file.getOriginalFilename(), file.getBytes());
+		
+		model.addAttribute("savedName", savedName);
+		
+		return "uploadResult";
+	}
+
+	private String uploadFile(String originalFilename, byte[] fileData) throws IOException {
+		UUID uid = UUID.randomUUID();
+		
+		String savedName = uid.toString() + "_" + originalFilename;
+		
+		File target = new File(uploadPath, savedName);
+		
+		FileCopyUtils.copy(fileData, target);
+		
+		return savedName;
+	}
+}
