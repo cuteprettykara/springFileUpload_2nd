74636829b67efafdf5f7ee6b382c1e2e4dc5e184
cuteprettykara <@gmail.com>
2018-04-26 11:31:33 +0900

    5_1_전송된 파일을 화면에 표시하기(파일데이터를 브라우저로 전송하기)

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,22 +1,30 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.util.MediaUtils;
 import org.zerock.util.UploadFileUtils;
 
 @Controller
@@ -78,5 +86,41 @@ public class UploadController {
 		String savedName= UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes());
 		
 		return new ResponseEntity<>(savedName, HttpStatus.CREATED);
+	}
+	
+	@ResponseBody
+	@RequestMapping("/displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName) throws Exception {
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		 
+		logger.info("File Name : {}", fileName);
+		logger.info("Full Name : {}", uploadPath+fileName);
+		 
+		try {
+			String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+			MediaType mType = MediaUtils.getMediaType(formatName);
+			HttpHeaders headers = new HttpHeaders();
+			 
+			in = new FileInputStream(uploadPath+fileName);
+			
+			if (mType == null) {
+				fileName = fileName.substring(fileName.indexOf("_") + 1);
+				headers.add("Content-Disposition", "attachment; filename=\"" + new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"" );
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+			} else {
+				headers.setContentType(mType);				
+			}
+			
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		} finally {
+			in.close();
+		}
+		 
+		return entity;
 	}
 }
